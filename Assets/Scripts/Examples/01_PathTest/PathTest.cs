bbdab534527a891a4d6b271e289269f97f3b20b1
JourneyHans <2749628228@qq.com>
2018-10-24 11:40:01 +0800

    新增工具 新增例子

---
+++ Assets/Scripts/Examples/01_PathTest/PathTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathTest : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Persistent Data Path: \t\t" + PathUtil.persistentDataPath);
+        Debug.Log("Assets Path: \t\t" + Application.dataPath);
+        Debug.Log("Resources Path: \t\t" + PathUtil.resourcePath);
+        Debug.Log("Streaming Assets Path: \t\t" + PathUtil.streamingAssetsPath);
+        Debug.Log("Application.temporaryCachePath: \t\t" + Application.temporaryCachePath);
+    }
+}
---
+++ Assets/Scripts/HzFramework/Utility/PathUtil.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 路径工具类
+/// </summary>
+public class PathUtil
+{
+    /// <summary>
+    /// 获取Resource路径
+    /// </summary>
+    public static string resourcePath
+    {
+        get
+        {
+            return Application.dataPath + "/Resources/";
+        }
+    }
+
+    /// <summary>
+    /// 获取数据存放目录（persistentDataPath）
+    /// </summary>
+    public static string persistentDataPath
+    {
+        get
+        {
+#if UNITY_ANDROID || UNITY_IOS
+            return Application.persistentDataPath + "/";
+#elif UNITY_EDITOR_OSX
+            int i = Application.dataPath.LastIndexOf('/');
+            return Application.dataPath.Substring(0, i + 1) + "/";
+#else
+            return Application.persistentDataPath + "/";
+#endif
+        }
+    }
+
+    /// <summary>
+    /// 获取streamingassets路径
+    /// </summary>
+    public static string streamingAssetsPath
+    {
+        get
+        {
+#if UNITY_EDITOR && !UNITY_EDITOR_OSX
+            return "file:///" + Application.streamingAssetsPath + "/";
+#elif UNITY_IOS
+            return "file://" + Application.streamingAssetsPath + "/";
+#elif UNITY_ANDROID
+            return Application.streamingAssetsPath + "/";
+#else
+            return "";
+#endif
+        }
+    }
+}
